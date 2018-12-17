16dfb9be6ab7cdfae3777996d3c1f8c27cb26794
8ptk4 <putte.karlsson@gmail.com>
2018-12-17 11:44:56 +0000

    Added a config file for Api keys to di container with a gitignore file to hide it.

--- config/di/keys.php
+++ config/di/keys.php
@@ -10,6 +10,7 @@ return [
             "callback" => function () {
                 $obj = [
                     "ipStack" => "hashdahsd",
+                    "darkSky" => "muahhaha",
                 ];
 
                 return $obj;
